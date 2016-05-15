8208b9fd96c17aa618d1aec3e868e33c258e9e08
Siyu Sun <sunsiyu.tud@gmail.com>
2016-05-15 22:17:28 +0200

    update vgrepl
    - add ... argument, now should align with grepl exactly
    - add more unit tests

--- R/vgrepl.R
+++ R/vgrepl.R
@@ -5,14 +5,14 @@
 #' @param operator either "or" or "and"
 #' @return logical vector of same length of vx
 #' @export
-vgrepl <- function(vpattern, vx, operator = "or") {
+vgrepl <- function(vpattern, vx, operator = "or", ...) {
 
   stopifnot(length(vpattern) > 0, length(vx) > 0)
 
   vpattern <- as.character(vpattern)
   vx <- as.character(vx)
 
-  lind <- lapply(vpattern, function(x) grepl(x, vx))
+  lind <- lapply(vpattern, function(x) grepl(x, vx, ...))
 
   if (operator == "or") {
     res <- Reduce("|", lind)
--- tests/testthat/test-vgrepl.R
+++ tests/testthat/test-vgrepl.R
@@ -1,4 +1,4 @@
-context("vgrepl")
+context("unit test: function vgrepl")
 
 ## TODO: Rename context
 ## TODO: Add more tests
@@ -10,3 +10,19 @@ test_that("vgrepl assertion works", {
 })
 
 
+test_that("vgrepl works in case of single pattern matching", {
+  expect_equal(vgrepl("a", "ab"), grepl("a", "ab"))
+  expect_equal(vgrepl("a", "ab"), grepl("a", "ab"))
+  expect_equal(vgrepl("a", c("ab", "ac", "bc")), grepl("a", c("ab", "ac", "bc")))
+})
+
+test_that("vgrepl behaves the same in case of NA", {
+  expect_equal(vgrepl(NA, "ab"), grepl(NA, "ab"))
+  expect_equal(vgrepl(NA, NA), grepl(NA, NA))
+  expect_equal(vgrepl(NA, c(NA, "a")), grepl(NA, c(NA, "a")))
+})
+
+test_that("vgrepl behaves the same in case of additional argument", {
+  expect_equal(vgrepl("a", c("ab", "AB"), ignore.case = T), grepl("a", c("ab", "AB"), ignore.case = T))
+  expect_equal(vgrepl(NA, c(NA, "a")), grepl(NA, c(NA, "a")))
+})
