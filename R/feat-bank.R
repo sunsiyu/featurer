0f6e8de8b13a4f8999df6aaf099c8b0f95205b45
Siyu Sun <sunsiyu.tud@gmail.com>
2016-07-10 20:09:57 +0200

    add feat-bank related empty structure

---
+++ R/feat-bank.R
@@ -0,0 +1,43 @@
+#' get IBAN features
+#'
+#' IBAN: international bank account number. originally adoppted by the european committee for banking standards (ECBS),
+#' and later under ISO 13616:1997/2007. SWIFT as the formal registrar. 69 countries were using IBAN numbering system.
+#' The IBAN consists of up to 34 alphanumeric characters: a country code, two check digits, a number that includes
+#' the domestic bank account number, branch identifier, and potential routing information.
+#' country code: two digits (ISO 3166-1 alpha-2)
+#' check digits: two digits, enable sanity check
+#' Basic Bank Account Number (BBAN) - up to 30 digits that are country-specific, no consistency between the formats.
+#' IBAN should not contain spaces.
+#' TODO
+getIBANfeatures <- function(x) {
+
+
+  return(x)
+
+}
+
+
+
+getSWIFTfeatures <- function(x) {
+
+
+  return(x)
+
+}
+
+getBICfeatures <- function(x) {
+
+
+  return(x)
+
+}
+
+
+
+getIINfeatures <- function(x) {
+
+
+  return(x)
+
+}
+
